5e0d4148993b4cef0c627a4a4e6971105a582c50
dnitin938 <69376461+dnitin938@users.noreply.github.com>
2025-06-16 17:49:43 +0530

    Update dff.py

--- dff.py
+++ dff.py
@@ -54,7 +54,7 @@ uploaded_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True,use_container_width=False)
 
     # ----------- PREPROCESS -----------
     transform = transforms.Compose([
